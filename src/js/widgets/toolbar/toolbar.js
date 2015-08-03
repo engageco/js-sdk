d0c72288f3e3596c0627909da9dfb41ae83bb4e1
Danny Patterson <me@dannypatterson.com>
2015-08-03 09:55:28 -0500

    added proactive bubble

--- src/js/widgets/toolbar/toolbar.js
+++ src/js/widgets/toolbar/toolbar.js
@@ -16,6 +16,7 @@ define("EngageToolbar", ["jquery",
             this.screenController.cssClasses.left = "engage-left";
             this.screenController.cssClasses.right = "engage-right";
 			jQuery(jQuery.proxy(onDOMReady, this));
+            this.neverOpened = true;
 		};
 
         EngageToolbar.SCREENS = {
@@ -32,15 +33,19 @@ define("EngageToolbar", ["jquery",
         };
 
 		var onDOMReady = function() {
+			var tabPlacement = (this.options.tabPlacement) ? this.options.tabPlacement : "right-tab";
 			// create tab
 			this.tab = jQuery('<div class="engage-tab">' +
                     '<div class="engage-tab-label"></div>' +
+                    '<div class="engage-profilePhoto engage-tiny">' +
+                        '<div class="engage-photo no-photo"><img></div>' +
+                        '<div class="engage-statusIndicator engage-online"></div>' +
+                    '</div>' +
                 '</div>');
-			this.tab.on("click", jQuery.proxy(onTabClick, this));
-			var tabPlacement = (this.options.tabPlacement) ? this.options.tabPlacement : "right-tab";
 			this.tab.addClass(tabPlacement);
 			if(this.options.backgroundColor) {
 				this.tab.css("background-color", this.options.backgroundColor);
+                this.tab.find(".engage-statusIndicator").css("border-color", this.options.backgroundColor);
 			}
 			var label = this.tab.find('.engage-tab-label');
 			var labelText = (this.options.label) ? this.options.label : "Chat";
@@ -53,6 +58,20 @@ define("EngageToolbar", ["jquery",
 			}
 			label.appendTo(this.tab);
 			this.tab.appendTo(jQuery("body"));
+            //console.log(this.tab.find(".engage-profilePhoto"));
+            this.tab.on("click", jQuery.proxy(onTabClick, this));
+            this.tab.find(".engage-profilePhoto").on("click", jQuery.proxy(onTabUserClick, this));
+
+            this.bubble = jQuery('<div class="engage-bubble">' +
+                    '<div class="engage-bubble-message">Hey there! Do you need any help?</div>' +
+                    '<div class="engage-name">Peter Hashtag</div>' +
+                    '<div class="engage-title">CTO</div>' +
+                    '<a class="engage-button engage-chat">Chat Now</a>' +
+                '</div>');
+            this.bubble.on("click", jQuery.proxy(onTabUserClick, this));
+            this.bubble.find(".engage-button").on("click", jQuery.proxy(onUserChatClick, this));
+            this.bubble.addClass(tabPlacement);
+            this.bubble.appendTo(jQuery("body"));
 
 			// create drawer
 			this.drawer = jQuery('<div class="engage-drawer">' +
@@ -101,6 +120,35 @@ define("EngageToolbar", ["jquery",
                 onUsersLoaded.apply(this, [this.users]);
             }
 		};
+
+        var onShowTabUser = function(user) {
+            //if(!this.bubble.hasClass("engage-show")) { // don't switch users if the proactive bubble is open
+                if(user && user.profilePhoto != null && user.profilePhoto != "") {
+                    this.tab.find(".engage-profilePhoto").data("user", user);
+                    this.tab.find(".engage-photo > img").attr("src", user.profilePhoto);
+                    this.tab.find(".engage-photo").removeClass("no-photo");
+                }else {
+                    this.tab.find(".engage-photo > img").attr("src", "");
+                    this.tab.find(".engage-photo").addClass("no-photo");
+                }
+                this.tab.toggleClass("engage-show-user", (user != null));
+                if(this.neverOpened && user && this.options.proactive) {
+                    this.bubble.data("user", user);
+                    this.bubble.find(".engage-name").text(user.firstName + " " + user.lastName);
+                    this.bubble.find(".engage-title").text(user.title);
+                    var proactiveDelay = this.options.proactive.delay ? this.options.proactive.delay : 100;
+                    clearTimeout(this.showProactiveBubbleTimeout);
+                    this.showProactiveBubbleTimeout = setTimeout(jQuery.proxy(onOpenProactiveBubble, this), proactiveDelay);
+                }else {
+                    this.bubble.removeClass("engage-show");
+                }
+            //}
+        };
+
+        var onOpenProactiveBubble = function() {
+            this.bubble.find(".engage-bubble-message").text(this.options.proactive.message);
+            this.bubble.addClass("engage-show");
+        };
 
 		var onUsersLoaded = function(data) {
 			this.users = data.users;
@@ -154,7 +202,8 @@ define("EngageToolbar", ["jquery",
                 button.toggleClass("engage-outline", !online);
                 button.text(label);
             }
-            // note: since this is called each time a user's presence changes, we want to add a delay in case multiple users change at once
+            // NOTE: Since this is called each time a user's presence changes, we want to add a delay
+            //       in case multiple users change at once.
             clearTimeout(this.sortTimeout);
             this.sortTimeout = setTimeout(jQuery.proxy(onSortUserList, this), 200);
 		};
@@ -173,10 +222,15 @@ define("EngageToolbar", ["jquery",
                     sortedList = SortOrderUtil.orderByLastChat(sortedList);
                     break;
             }
-            if(this.options.showOnlineAgentsFirst) {
+            if(this.options.showOnlineAgentsFirst != false) {
                 sortedList = SortOrderUtil.orderByOnline(sortedList, this.sdk.presence);
             }
             this.directoryScreen.find("ul").append(sortedList);
+            //if(this.currentUser == null || this.sdk.presence.getUserStatus(this.currentUser.domain) != "online") {
+                var user = sortedList.first().data("user");
+                var isOnline = this.sdk.presence.getUserStatus(user.domain) == "online";
+                onShowTabUser.apply(this, [isOnline ? user : null]);
+            //}
         };
 
 		var onUserClick = function(event) {
@@ -184,8 +238,13 @@ define("EngageToolbar", ["jquery",
             this.screenController.setScreen(EngageToolbar.SCREENS.PROFILE);
             this.drawer.find(".engage-back").removeClass("engage-hide");
             this.currentUser = user;
+            var status = this.sdk.presence.getUserStatus(user.domain);
+            if(status == "online") {
+                onShowTabUser.apply(this, [this.currentUser]);
+            }
             if(user.profilePhoto != null && user.profilePhoto != "") {
                 this.profileScreen.find(".engage-photo > img").attr("src", user.profilePhoto);
+                this.tab.find(".engage-photo").removeClass("no-photo");
             }else {
                 this.profileScreen.find(".engage-photo > img").attr("src", "");
                 this.profileScreen.find(".engage-photo").addClass("no-photo");
@@ -197,7 +256,7 @@ define("EngageToolbar", ["jquery",
             var bio = jQuery.trim(user.bio).substring(0, 140).split(" ").slice(0, -1).join(" ");
             if(bio != "") bio += "...";
             this.profileScreen.find(".engage-bio").text(bio);
-            onUserPresenceChange.apply(this, [this.sdk.presence.getUserStatus(user.domain), user.domain]);
+            onUserPresenceChange.apply(this, [status, user.domain]);
 		};
 
 		var onUserChatClick = function(event) {
@@ -208,32 +267,44 @@ define("EngageToolbar", ["jquery",
 
 		var onTabClick = function(event) {
 			this.tab.addClass("engage-hide");
+            clearTimeout(this.showProactiveBubbleTimeout);
+            this.bubble.removeClass("engage-show");
+            this.neverOpened = false;
 			var self = this;
 			setTimeout(function() {
 				self.drawer.removeClass("engage-hide");
-                setTimeout(function() {
-                    if(self.screenController.currentScreen == null) {
-                        if(self.options.showSearch) {
-                            self.screenController.setScreen(EngageToolbar.SCREENS.SEARCH);
-                        }else {
-                            self.screenController.setScreen(EngageToolbar.SCREENS.DIRECTORY);
-                        }
-                    }
-                }, 300);
+                setTimeout(jQuery.proxy(onShowFirstScreen, self), 300);
 			}, 300);
 		};
 
-		var onDrawerCloseClick = function(event) {
-			this.drawer.addClass("engage-hide");
-			var self = this;
-			setTimeout(function() {
-				self.tab.removeClass("engage-hide");
-			}, 500);
-		};
+        var onTabUserClick = function(event) {
+            event.stopImmediatePropagation();
+            onShowFirstScreen.apply(this);
+            onTabClick.apply(this, [event]);
+            onUserClick.apply(this, [event]);
+        };
+
+        var onDrawerCloseClick = function(event) {
+            this.drawer.addClass("engage-hide");
+            var self = this;
+            setTimeout(function() {
+                self.tab.removeClass("engage-hide");
+            }, 500);
+        };
 
         var onScreenBackClick = function(event) {
             this.screenController.back();
             this.drawer.find(".engage-back").toggleClass("engage-hide", this.screenController.currentIndex == 0);
+        };
+
+        var onShowFirstScreen = function() {
+            if(this.screenController.currentScreen == null) {
+                if(this.options.showSearch) {
+                    this.screenController.setScreen(EngageToolbar.SCREENS.SEARCH);
+                }else {
+                    this.screenController.setScreen(EngageToolbar.SCREENS.DIRECTORY);
+                }
+            }
         };
 
 		EngageToolbar.prototype.setVisibility = function(isVisible) {
