75b3944fc8af39051686e1b76aef9b58f3b286fa
Danny Patterson <me@dannypatterson.com>
2016-09-29 23:10:17 -0500

    added debugging code

--- build/toolbar.js
+++ build/toolbar.js
@@ -172,7 +172,7 @@ EngageToolbar = function (jQuery, ScreenController, SortOrderUtil) {
     this.callback = callback;
     jQuery(jQuery.proxy(onDOMReady, this));
   };
-  EngageToolbar.version = '1.0.99';
+  EngageToolbar.version = '1.0.100';
   EngageToolbar.SCREENS = {
     SEARCH: 'search',
     DIRECTORY: 'directory',
